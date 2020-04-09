717aec629807bb43d9f04f5bac457e3d2b081e95
Rakesh <rakesht2499@gmail.com>
2020-04-09 19:51:56 +0530

    Adding IP validation

--- Ipv4.py
+++ Ipv4.py
@@ -6,19 +6,29 @@ class Ipv4:
         self.head = RadixTree("N")
 
     def add(self, ip) -> bool:
-        bin_ip = self._to_binary(ip)
-        return self.head.add(bin_ip)
+        if self._validate_ip(ip):
+            bin_ip = self._to_binary(ip)
+            return self.head.add(bin_ip)
+        return False
 
     def remove(self, ip) -> bool:
-        bin_ip = self._to_binary(ip)
-        return self.head.remove(bin_ip)
+        if self._validate_ip(ip):
+            bin_ip = self._to_binary(ip)
+            return self.head.remove(bin_ip)
+        return False
 
     def is_present(self, ip) -> bool:
-        bin_ip = self._to_binary(ip)
-        return self.head.is_present(bin_ip)
+        if self._validate_ip(ip):
+            bin_ip = self._to_binary(ip)
+            return self.head.is_present(bin_ip)
+        return False
 
     def _to_binary(self, ip):
         binary_ip = ""
         for x in ip.split("."):
             binary_ip += f'{int(x):08b}'
         return binary_ip
+
+    def _validate_ip(self, ip):
+        valid = sum([1 for bit8 in ip.split(".") if 255 >= int(bit8) >= 0]) == 4
+        return valid
